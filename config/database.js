e9ecb9d593c3945cf303664d4b0962f6671b6b1a
abansal <abansal.shutterfly.com>
2017-03-19 19:25:34 +0530

    added heroku db url

--- config/database.js
+++ config/database.js
@@ -1,4 +1,6 @@
 module.exports = {
 
-    'url' : 'mongodb://localhost:27017/capi' 
+    //'url' : 'mongodb://localhost:27017/capi' 
+        'url' : 'mongodb://prithviraju1369:prithviraju1369@ds135690.mlab.com:35690/capillary' 
+
 };
